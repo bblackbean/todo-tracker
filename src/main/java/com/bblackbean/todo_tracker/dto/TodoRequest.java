605673ff1132b2d9282b379e53c6e0eb41ce311a
bblackbean <smart13210@gmail.com>
2025-11-04 11:15:06 +0900

    TodoRequest DTO에 startDate, endDate 필드 추가

--- src/main/java/com/bblackbean/todo_tracker/dto/TodoRequest.java
+++ src/main/java/com/bblackbean/todo_tracker/dto/TodoRequest.java
@@ -2,10 +2,14 @@ package com.bblackbean.todo_tracker.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor     // 모든 필드 값을 받는 생성자 자동 생성
 @NoArgsConstructor      // 기본 생성자 자동 생성
@@ -18,4 +22,12 @@ public class TodoRequest {
 
     @Schema(description = "완료여부")
     private boolean completed;
+
+    @NotNull(message = "시작일을 입력하세요.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @NotNull(message = "종료일을 입력하세요.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
 }

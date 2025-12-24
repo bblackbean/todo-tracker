6699d7de33822bbcb73a5945690a8e3209b07902
bblackbean <smart13210@gmail.com>
2025-12-24 14:27:37 +0900

    주석 제거

--- src/main/java/com/bblackbean/todo_tracker/controller/WeatherProxyController.java
+++ src/main/java/com/bblackbean/todo_tracker/controller/WeatherProxyController.java
@@ -20,29 +20,10 @@ public class WeatherProxyController {
 
     private final WeatherService weatherService;
 
-//    @Value("${openweathermap.api.key}")
-//    private String weatherApiKey;
-
-    // final로 선언하여 @RequiredArgsConstructor가 생성자를 만들도록 함
-//    private final RestTemplate restTemplate;
-
     @GetMapping("/weather")
     public ResponseEntity<String> getCurrentWeather(@RequestParam("lat") double lat, @RequestParam("lon") double lon) {
         log.info("Fetching weather for lat: {}, lon: {}", lat, lon);
-
-//        String url = "https://api.openweathermap.org/data/2.5/weather";
-//
-//        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url)
-//                .queryParam("lat", lat)     // 위도
-//                .queryParam("lon", lon)     // 경도
-//                .queryParam("appid", weatherApiKey)
-//                .queryParam("units", "metric") // 섭씨온도 사용
-//                .queryParam("lang", "kr");     // 한국어 설명 사용
-
         try {
-//            ResponseEntity<String> response = restTemplate.getForEntity(builder.toUriString(), String.class);
-//            return ResponseEntity.ok(response.getBody());
-
             String body = weatherService.getCurrentWeather(lat, lon);
             return ResponseEntity.ok(body);
         } catch (Exception e) {
